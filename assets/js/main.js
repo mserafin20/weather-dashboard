8209b8cd5deb97a3176779e0c614f65160855f73
mserafin20 <mmserafin20@gmail.com>
2023-04-27 16:01:11 -0400

    added dayjs for displaying the dates on the forecasts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
+// API Key: a61eec67c6450e095a71a7fe664a1688
+//Variable List
+
+//Utilizing dayjs to display the date on the page
+var currentDate = dayjs();
+$('#current-date').text(currentDate.format("MM D, YYYY"));
+
+var firstDay = currentDate.add(1, 'day');
+$('#first-day').text(firstDay.format("MM D, YYYY")); 
+
+var secondDay = currentDate.add(1, 'day');
+$('#second-day').text(secondDay.format("MM D, YYYY"));
+
+var thirdDay = currentDate.add(1, 'day');
+$('#third-day').text(thirdDay.format("MM D, YYYY"));
+
+var fourthDay = currentDate.add(1, 'day');
+$('#fourth-day').text(fourthDay.format("MM D, YYYY"));
+
+var fifthDay = currentDate.add(1, 'day');
+$('#fifth-day').text(fifthDay.format("MM D, YYYY"));
+
+
 // Fetching information from weather api
 
-
+// Clear button function
+clearData.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
+});
 
 // Initial function
 function init() {
