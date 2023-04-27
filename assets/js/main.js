d067aea184aaf109ccf9cc1be8edfbcbaee60171
mserafin20 <mmserafin20@gmail.com>
2023-04-27 15:20:38 -0400

    added initial function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 // Fetching information from weather api
+
+
+
+// Initial function
+function init() {
+    var cityList = [];
+    cityList = JSON.parse(localStorage.getItem("searchedCities"));
+    if(cityList !== null) {
+        for(var i = 0; i < cityList.length; i++) {
+            var listEl = document.createElement("button");
+            listEl.textContent = cityList[i];
+            listEl.setAttribute("class", "col-12 col-md-8");
+            listEl.setAttribute("style", "background-color: rgb(9, 133, 235); color: aliceblue; border-style: solid black 2px; margin-top: 5px")
+            searchData.append(listEl);
+            listEl.addEventListener("click", searchHistoryBtn);
+        }
+    }
+}
+
+init();
