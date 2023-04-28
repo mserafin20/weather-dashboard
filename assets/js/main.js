3c75cba40239a3f6e67d7780e2e8962beb067aec
mserafin20 <mmserafin20@gmail.com>
2023-04-27 21:23:19 -0400

    added the local storge feature, still having api key issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ var currentDate = dayjs();
 $('#current-date').text(currentDate.format("MM D, YYYY"));
 
 var firstDay = currentDate.add(1, 'day');
-$('#first-day').text(firstDay.format("MM D, YYYY")); 
+$('#first-day').text(firstDay.format("MM D, YYYY"));
 
 var secondDay = currentDate.add(1, 'day');
 $('#second-day').text(secondDay.format("MM D, YYYY"));
@@ -54,7 +54,7 @@ $('#fifth-day').text(fifthDay.format("MM D, YYYY"));
 // Fetching information from weather api
 
 // Clear button function
-clearBtn.addEventListener("click", function() {
+clearBtn.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
@@ -63,8 +63,8 @@ clearBtn.addEventListener("click", function() {
 function init() {
     var cityList = [];
     cityList = JSON.parse(localStorage.getItem("searchedCities"));
-    if(cityList !== null) {
-        for(var i = 0; i < cityList.length; i++) {
+    if (cityList !== null) {
+        for (var i = 0; i < cityList.length; i++) {
             var listEl = document.createElement("button");
             listEl.textContent = cityList[i];
             listEl.setAttribute("class", "col-12 col-md-8");
@@ -81,12 +81,51 @@ function searchHistoryBtn() {
     var cityName = (this.textContent);
     // Fetch information from openweather api. 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid={a61eec67c6450e095a71a7fe664a1688}`)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    });
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
     .then(function (data) {
-        console.log(data);
+            console.log(data);
+            //Updates info about the current day
+            currentCity.textContent = cityName
+            currentTemp.textContent = `${data.list[0].main.temp}`;
+            currentWind.textContent = `${data.list[0].wind.speed}`;
+            currentHumidity.textContent = `${data.list[0].main.humidity}`;
+            // Five day forecast
+            firstDayTemp.textContent = `${data.list[6].main.temp}`;
+            firstDayWind.textContent = `${data.list[6].wind.speed}`;
+            firstDayHumidity.textContent = `${data.list[6].main.humidity}`;
+
+            secondDayTemp.textContent = `${data.list[14].main.temp}`;
+            secondDayWind.textContent = `${data.list[14].wind.speed}`;
+            secondDayHumidity.textContent = `${data.list[14].main.humidity}`;
+
+            thirdDayTemp.textContent = `${data.list[22].main.temp}`;
+            thirdDayWind.textContent = `${data.list[22].wind.speed}`;
+            thirdDayHumidity.textContent = `${data.list[22].main.humidity}`;
+
+            fourthDayTemp.textContent = `${data.list[30].main.temp}`;
+            fourthDayWind.textContent = `${data.list[30].wind.speed}`;
+            fourthDayHumidity.textContent = `${data.list[30].main.humidity}`;
+
+            fifthDayTemp.textContent = `${data.list[38].main.temp}`;
+            fifthDayWind.textContent = `${data.list[38].wind.speed}`;
+            fifthDayHumidity.textContent = `${data.list[38].main.humidity}`;
+
+
+
+
+
+        })
+};
+// Fetching data about the default city, "Philadelphia"
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=philadelphia&appid={a61eec67c6450e095a71a7fe664a1688}`)
+
+    .then(function (answer) {
+        return answer.json();
+    })
+    .then(function (data) {
         //Updates info about the current day
         currentCity.textContent = cityName
         currentTemp.textContent = `${data.list[0].main.temp}`;
@@ -112,21 +151,67 @@ function searchHistoryBtn() {
         fifthDayTemp.textContent = `${data.list[38].main.temp}`;
         fifthDayWind.textContent = `${data.list[38].wind.speed}`;
         fifthDayHumidity.textContent = `${data.list[38].main.humidity}`;
+    });
 
+//Function for the search button
+function getWeather() {
+    var nameCity = citySearch.value;
+    console.log(nameCity)
+    console.log(citySearch.value);
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid={a61eec67c6450e095a71a7fe664a1688}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //Updates info about the current day
+            currentCity.textContent = cityName
+            currentTemp.textContent = `${data.list[0].main.temp}`;
+            currentWind.textContent = `${data.list[0].wind.speed}`;
+            currentHumidity.textContent = `${data.list[0].main.humidity}`;
+            // Five day forecast
+            firstDayTemp.textContent = `${data.list[6].main.temp}`;
+            firstDayWind.textContent = `${data.list[6].wind.speed}`;
+            firstDayHumidity.textContent = `${data.list[6].main.humidity}`;
 
+            secondDayTemp.textContent = `${data.list[14].main.temp}`;
+            secondDayWind.textContent = `${data.list[14].wind.speed}`;
+            secondDayHumidity.textContent = `${data.list[14].main.humidity}`;
 
+            thirdDayTemp.textContent = `${data.list[22].main.temp}`;
+            thirdDayWind.textContent = `${data.list[22].wind.speed}`;
+            thirdDayHumidity.textContent = `${data.list[22].main.humidity}`;
 
+            fourthDayTemp.textContent = `${data.list[30].main.temp}`;
+            fourthDayWind.textContent = `${data.list[30].wind.speed}`;
+            fourthDayHumidity.textContent = `${data.list[30].main.humidity}`;
 
-    })
+            fifthDayTemp.textContent = `${data.list[38].main.temp}`;
+            fifthDayWind.textContent = `${data.list[38].wind.speed}`;
+            fifthDayHumidity.textContent = `${data.list[38].main.humidity}`;
+        })
+
+        //Saving search history into local storage
+        var citiesList = JSON.parse(localStorage.getItem("searched-cities")) || [];
+        citiesList.push(citySearch.value);
+        localStorage.setItem("searchedCities", JSON.stringify(citiesList));
+        //Displaying the search history in the local storage onto the site
+        for(var i = 0; i < cityList.length; i++) {
+            var citiesList = [];
+            citiesList.push(citySearch.value);
+            //Making any previous search into a button
+            var list = document.createElement("button");
+            list.textContent = citiesList[i];
+            list.setAttribute("class", "col-12 col-md-8");
+            list.setAttribute("style", "background-color: rgb(9, 133, 235); color: aliceblue; border-style: solid black 2px; margin-top: 5px");
+            //appending buttons
+            searchData.append(list);
+        }
+
 };
-// Fetching data about the default city, "Philadelphia"
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=philadelphia&appid={a61eec67c6450e095a71a7fe664a1688}`)
 
-.then(function (answer) {
-    return answer.json();
-})
+searchBtn.addEventListener("click", getWeather);
 
- 
+
 
 
 
